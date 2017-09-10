8c4b8fa04f74160b2e2f5471da4f044ddf8ae269
Jacob Killelea <Jkillelea@protonmail.ch>
2017-09-10 12:02:40 -0600

    estimate drag as well

--- estimate_lift.m
+++ estimate_lift.m
@@ -1,4 +1,5 @@
-function lift = estimate_lift(cp_upper, cp_lower, alpha, n_iters)
+function [lift, drag] = estimate_lift(cp_upper, cp_lower, alpha, n_iters)
   [n, a] = estimate_n_a(cp_upper, cp_lower, n_iters);
   lift = n*cosd(alpha) - a*sind(alpha);
+  drag = n*sind(alpha) + a*cosd(alpha);
 end
