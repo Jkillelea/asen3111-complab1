f70016a312bf5adf668d5f895f94ee78353d18d1
Jacob Killelea <Jkillelea@protonmail.ch>
2017-09-11 12:36:14 -0600

    Change cylinder to radius of 1 m, make code prettier

--- main.m
+++ main.m
@@ -4,7 +4,7 @@
 % FIRST PROBLEM - Sphere in inviscid flow
 clear all; clc;
 
-radius  = 10;                        % meters
+radius  = 1;                        % meters
 v_inf   = 25;                        % m/s
 rho_inf = 0.9093;                    % kg/m^3
 p_inf   = 7.012 * (10^4);            % Pa
@@ -25,27 +25,20 @@ range   = [0, 2*pi];
 h       = (2*pi)/N_iters;
 t       = linspace(range(1), range(2), N_iters+1);
 
-% Calculate lift from pressure distribution
-accumulator = 0;
-f = up_force;
+% Calculate lift and drag from pressure distribution
+lift_accum = 0;
+drag_accum = 0;
+lif = up_force;
+drg = side_force;
 for k = 1:(N_iters/2) % simpson's method for a line integral around a sphere
   t_1 = t(2*k-1);
   t_2 = t(2*k);
   t_3 = t(2*k+1);
-  accumulator = accumulator + ( f(t_1) + 4*f(t_2) + f(t_3) );
+  lift_accum = lift_accum + ( lif(t_1) + 4*lif(t_2) + lif(t_3) );
+  drag_accum = drag_accum + ( drg(t_1) + 4*drg(t_2) + drg(t_3) );
 end
-lift = (h*R/3) * accumulator;
-
-% Calculate drag from pressure distribution
-accumulator = 0;
-f = side_force;
-for k = 1:(N_iters/2)
-  t_1 = t(2*k-1);
-  t_2 = t(2*k);
-  t_3 = t(2*k+1);
-  accumulator = accumulator + ( f(t_1) + 4*f(t_2) + f(t_3) );
-end
-drag = (h*R/3) * accumulator;
+lift = (h*R/3) * lift_accum;
+drag = (h*R/3) * drag_accum;
 
 fprintf('Sphere of radius %d m: lift: %d N, drag: %d N.\n', radius, lift, drag);
 
@@ -56,12 +49,17 @@ clear all;
 load Cp;
 
 % airfoil polynomial
-airfoil = @(x, c) (0.12 .* c ./ 0.2) .* (0.2969.*sqrt(x./c) - 0.126.*sqrt(x./c) - 0.3516.*(x./c).^2 + 0.2843.*(x./c).^3 - 0.1036.*(x./c).^4);
+airfoil = @(x, c) (0.12 .* c ./ 0.2) .* (0.2969.*sqrt(x./c)  ...
+                                       - 0.126.*sqrt(x./c)   ...
+                                       - 0.3516.*(x./c).^2   ...
+                                       + 0.2843.*(x./c).^3   ...
+                                       - 0.1036.*(x./c).^4);
 
 alpha = 9; % degrees
 chord = 0.5; % meters
 airfoil = @(x) airfoil(x, chord);
 
+disp('Calculating lift and drag. This may take a bit...');
 truth_iterations = 50000; % fifty thousand iterations for 'truth' measurement
 [true_lift, true_drag] = estimate_lift(Cp_upper, Cp_lower, alpha, truth_iterations);
 
